fc8f85dddab7d796c0b162562dd71db29075c142
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2023-09-29 09:11:59 -0400

    added Math functions

---
+++ Unit One - Introduction to Java/src/lesson6/Example1.java
@@ -0,0 +1,13 @@
+package lesson6;
+
+public class Example1 {
+    public static void main(String[] args) {
+        int x = 10;
+        double z = Math.sqrt(x);    // square root
+        
+        System.out.println(z);
+
+
+
+    }
+}
