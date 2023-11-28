f0c1dd22f442ed0ff14858a27547992a253abba3
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2023-11-28 08:33:17 -0500

    NestedLoops

---
+++ Unit Four - Iteration with Loops/src/lesson3/NestedLoops.java
@@ -0,0 +1,18 @@
+package lesson3;
+
+/**
+ * NestedLoops
+ */
+public class NestedLoops {
+    public static void main(String[] args) {
+        rectangle(5,4);
+        triangle(5,4);
+    }
+
+    private static void triangle(int height, int base) {
+    }
+
+    private static void rectangle(int rows, int cols) {
+    }
+    
+}
