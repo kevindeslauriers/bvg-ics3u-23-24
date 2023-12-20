880115f5a2de5f16e8cf178e31a3172de10ce90a
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2023-12-20 11:29:45 -0500

    Steptracker

---
+++ Unit Five - Writing Classes/src/FRQ/StepTracker.java
@@ -0,0 +1,33 @@
+package FRQ;
+
+public class StepTracker {
+    private int minSteps;
+    private int numActiveDays;
+    private int numDays;
+    private int totalSteps;
+
+    public StepTracker(int minSteps){
+        this.minSteps = minSteps;
+        numActiveDays = 0;
+        numDays = 0;
+        totalSteps = 0;
+    }
+ 
+    public int activeDays(){
+        return numActiveDays;
+    }
+
+    public double averageSteps(){
+        if (numDays == 0)
+            return 0.0;
+        return totalSteps / (double) numDays;
+    }
+
+    public void addDailySteps(int steps){
+        totalSteps += steps;
+        if (steps >= minSteps)
+            numActiveDays++;
+        numDays++;
+    }
+
+}
