2962ce3e5e914f7bde23721d7e66453e2491b787
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2024-04-10 09:09:14 -0400

    Inheritance

---
+++ Unit Nine - Inheritance/src/Driver.java
@@ -0,0 +1,10 @@
+public class Driver {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+
+        Android a = new Android(x);  // call the Android 1 arg constructor
+        a.setServoCount(y);
+        System.out.println(a.getServoCount());
+    }
+}
