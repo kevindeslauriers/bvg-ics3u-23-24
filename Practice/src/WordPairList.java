05201f2f5be34f06781cbfa2ce0d3f5616ff960b
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2024-05-06 09:35:35 -0400

    fix in wordpairlist constructor

--- Practice/src/WordPairList.java
+++ Practice/src/WordPairList.java
@@ -6,7 +6,7 @@ public class WordPairList {
     public WordPairList(String[] words){
         allPairs = new ArrayList<WordPair>();
         for (int i = 0; i < words.length-1; i++) {
-            for (int j = i; j < words.length; j++) {
+            for (int j = i+1; j < words.length; j++) {
                 allPairs.add(new WordPair(words[i], words[j]));
             }
             
